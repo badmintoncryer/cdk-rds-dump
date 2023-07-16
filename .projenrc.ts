0990f858dc099fe9e0bc7270e0eb6813974597b8
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2023-07-16 21:46:02 +0900

    projen

---
+++ .projenrc.ts
@@ -0,0 +1,36 @@
+import { awscdk } from "projen";
+import { ReleaseTrigger } from "projen/lib/release";
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: "Kazuho CryerShinozuka",
+  authorAddress: "malaysia.cryer@gmail.com",
+  buildWorkflow: false,
+  cdkVersion: "2.87.0",
+  defaultReleaseBranch: "main",
+  github: false,
+  jsiiVersion: "~5.0.0",
+  name: "cdk-rds-dump",
+  projenrcTs: true,
+  repositoryUrl: "https://github.com/malaysia.cryer/cdk-rds-dump.git",
+  description: "CDK Construct Library by Typescript for RDS Dump",
+  python: {
+    distName: "cdk-rds-dump",
+    module: "cdk_rds_dump",
+  },
+  dotnet: {
+    dotNetNamespace: "CdkRdsDump",
+    packageId: "CdkRdsDump",
+  },
+  publishToMaven: {
+    javaPackage: "com.github.malaysia-cryer.cdk-rds-dump",
+    mavenGroupId: "com.github.malaysia-cryer",
+    mavenArtifactId: "cdk-rds-dump",
+  },
+  majorVersion: 1,
+  releaseTrigger: ReleaseTrigger.manual(),
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
